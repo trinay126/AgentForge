4daaa4f688dae2ce1a025a24a7605e16c6e615d3
trinay126 <thrinaychadalavada@gmail.com>
2026-08-18 18:52:12 +0530

    added Dunder methods and completed registry.py

--- utils/registry.py
+++ utils/registry.py
@@ -31,4 +31,28 @@ class Registry:
     def remove(self, name):
         if name in self._items:
             del self._items[name]
-            
+
+    #--Dunder methods -----------------------------------------------------------------------------
+    def __contains__(self, name):
+        return name in self._items
+
+    def __getitem__(self, name):
+        if name not in self._items:
+            raise KeyError(f"{self.registry_type} '{name}' not found"
+                           f"Available: {self.all_names()}")
+
+        return self._items[name]
+
+    def __len__(self):
+        return len(self._items)
+
+    def __iter__(self):
+        return iter(self._items.values())
+
+    def __str__(self):
+        names = ",".join(self._items.keys())
+        return f"registry({self.registry_type})[{len(self)}]: {names}"
+
+    def __repr__(self):
+        return f"Registry(type={self.registry_type!r}, count={len(self)})"
+        
